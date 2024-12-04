be541cf343db464b286560579127dd9ed9722588
Esmaeil Roohparvar Basmnej <e.roohparvar@modares.ac.ir>
2024-12-04 15:13:08 +0330

    Add code to Theoretical pI of protein sequences

--- Codes/Result.php
+++ Codes/Result.php
@@ -32,7 +32,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             }
         }
 
-        // وزن مولکولی هر آمینواسید (به گرم/مول)
+
         $aminoAcidWeights = [
             'A' => 89.09,  'R' => 174.20, 'N' => 132.12, 'D' => 133.10,
             'C' => 121.15, 'E' => 147.13, 'Q' => 146.15, 'G' => 75.07,
@@ -41,11 +41,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             'T' => 119.12, 'W' => 204.23, 'Y' => 181.19, 'V' => 117.15
         ];
 
+        $aminoAcids = array(
+            'D' => 3.8949375, 'E' => 4.2930625,
+            'H' => 5.8028125, 'C' => 7.75075,
+            'Y' => 9.4375625, 'K' => 10.4536875,
+            'R' => 12.0601875
+        );
+
         echo "<h2>Processed Protein Sequences:</h2>";
         foreach ($proteinSequences as $proteinName => $sequence) {
             $length = strlen($sequence);
 
-            // محاسبه وزن مولکولی
+
             $molecularWeight = 0.0;
             foreach (str_split($sequence) as $aminoAcid) {
                 if (isset($aminoAcidWeights[$aminoAcid])) {
@@ -56,7 +63,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             echo "<h3>$proteinName (Length: $length, Molecular Weight: " . number_format($molecularWeight, 2) . " g/mol)</h3>";
             echo "<pre>$sequence</pre>";
 
-            // Count occurrences of each amino acid
+
             $aminoAcidCounts = count_chars($sequence, 1);
 
             echo "<h4>Amino Acid Composition:</h4>";
@@ -70,6 +77,38 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             }
 
             echo "</table><br>";
+
+
+            $charge = array();
+
+
+            for ($pH = 0; $pH <= 14; $pH++) {
+                $charge[$pH] = 0;
+
+                for ($i = 0; $i < strlen($sequence); $i++) {
+                    $aminoAcid = $sequence[$i];
+                    if (isset($aminoAcids[$aminoAcid])) {
+                        if ($aminoAcid == "D" || $aminoAcid == "E") {
+                            $pK = $aminoAcids[$aminoAcid];
+                            $charge[$pH] += (1 / (1 + pow(10, $pK - $pH)));
+                        } else {
+                            $pK = $aminoAcids[$aminoAcid];
+                            $charge[$pH] += (1 / (1 + pow(10, $pH - $pK)));
+                        }
+                    }
+                }
+            }
+
+            $minDiff = 999999;
+            $pI = 0;
+            for ($pH = 0; $pH <= 14; $pH++) {
+                if (abs($charge[$pH]) < $minDiff) {
+                    $minDiff = abs($charge[$pH]);
+                    $pI = $pH;
+                }
+            }
+
+            echo "<h4>Theoretical pI of $proteinName: " . number_format($pI, 2) . "</h4><br>";
         }
     } else {
         echo "<h2>No input provided!</h2>";
